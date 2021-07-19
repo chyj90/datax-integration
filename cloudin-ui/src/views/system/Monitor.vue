a5aaa33d02abd59cd6df0a891bd6fda7c23fd528
chengyajie <18939578237@163.com>
2021-07-19 08:47:58 +0800

    增加系统监控对节点内存线程进行监控

--- cloudin-ui/src/views/system/Monitor.vue
+++ cloudin-ui/src/views/system/Monitor.vue
@@ -1,5 +1,14 @@
 <template>
   <page-header-wrapper>
+    <div>
+      <div :style="{ borderBottom: '1px solid #E9E9E9' }">
+        <a-checkbox :indeterminate="indeterminate" :checked="checkAll" @change="onCheckAllChange">
+          全选/反选
+        </a-checkbox>
+      </div>
+      <br />
+      <a-checkbox-group v-model="checkedList" :options="metrics" @change="onChange" />
+    </div>
     <s-table
       ref="table"
       size="default"
@@ -19,6 +28,8 @@ export default {
   },
   data () {
     return {
+      checkAll: false,
+      indeterminate: false,
       fixcolumns: {
         title: '服务名',
         dataIndex: 'seqId',
@@ -33,6 +44,17 @@ export default {
   computed: {},
   watch: {},
   methods: {
+    onChange (checkedList) {
+      this.indeterminate = !!checkedList.length && checkedList.length < this.metrics.length
+      this.checkAll = checkedList.length === this.metrics.length
+    },
+    onCheckAllChange (e) {
+      Object.assign(this, {
+        checkedList: e.target.checked ? this.metrics : [],
+        indeterminate: false,
+        checkAll: e.target.checked
+      })
+    },
     getMetrics () {
       metricList().then((res) => {
         this.metrics = res
